7cf7d8d58b54f882e8a533a76e89e5dd6759db22
Sofia-Tohme-Globant <sofia.tohme@globant.com>
2025-02-12 15:29:51 -0300

    [ADD] DrawerNavigator

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <View style={style.mainContainer}>
       <Title title={"Let's Explore"} />
-      <TouchableOpacity style={style.messageIconAndNumberContainer} onPress={() => navigation.navigate(Routes.Profile as never)}>
+      <TouchableOpacity style={style.messageIconAndNumberContainer}>
         <FontAwesomeIcon icon={faEnvelope} color={'#898DAE'} size={20} />
         <View style={style.messageNumberContainer}>
           <Text style={style.messageNumber}>2</Text>
--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -1,20 +1,34 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { Routes } from './Routes';
+//import {createStackNavigator} from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Routes} from './Routes';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const MainNavigation = () =>{
-    return(
-        <Stack.Navigator
-          screenOptions={{header: () => null, headerShown: false}}
-          initialRouteName={Routes.Home}
-        >
-          <Stack.Screen name={Routes.Home} component={Home} />
-          <Stack.Screen name={Routes.Profile} component={Profile} />
-        </Stack.Navigator>
-    )
-}
+
+
+const MainNavigation = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{header: () => null, headerShown: false}}
+      initialRouteName={Routes.Home}
+    >
+      <Stack.Screen name={'Drawer'} component={MainMenuNavigation} />
+    </Stack.Navigator>
+  );
+};
+
+const MainMenuNavigation = () => {
+  return (
+    <Drawer.Navigator screenOptions={{header: () => null, headerShown: false}}>
+      <Drawer.Screen name={Routes.Home} component={Home} />
+      <Drawer.Screen name={Routes.Profile} component={Profile} />
+    </Drawer.Navigator>
+  );
+};
 
 export default MainNavigation;
